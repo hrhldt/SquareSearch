242436844909d06247c10efdaf87626e63360727
Martin Herholdt Rasmussen <herholdt89@gmail.com>
2016-03-01 10:18:27 +0100

    Added a few tests

--- FourSquareSearchTests/FourSquareSearchTests.m
+++ FourSquareSearchTests/FourSquareSearchTests.m
@@ -7,16 +7,35 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "FSNetwork.h"
 
-@interface FourSquareSearchTests : XCTestCase
+@interface FourSquareSearchTests : XCTestCase <FSNetworkDelegate>
 
 @end
 
 @implementation FourSquareSearchTests
 
+NSInteger kNetworkTimeout = 20;
+XCTestExpectation *getVenuesExpectation;
+XCTestExpectation *getVenuesShouldFailExpectation;
+
+NSString *latLngQuery;
+NSString *searchQuery;
+
+NSString *requestShouldSucceed = @"Request should succeed";
+NSString *requestShouldFail = @"Request should fail";
+
 - (void)setUp {
     [super setUp];
+    
+    latLngQuery = @"55.12,12.56";
+    searchQuery = @"s";
+    
     // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)waitForExpectationsWithCommonTimeoutUsingHandler:(XCWaitCompletionHandler)handler {
+    [self waitForExpectationsWithTimeout:kNetworkTimeout handler:handler];
 }
 
 - (void)tearDown {
@@ -24,16 +43,28 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testThatGetVenuesFailsWhenParsingInWrongLocation {
+    getVenuesShouldFailExpectation = [self expectationWithDescription:requestShouldFail];
+    [FSNetwork requestPlacesWithSearchQuery:@"" latLngQuery:nil delegate:self];
+    [self waitForExpectationsWithCommonTimeoutUsingHandler:nil];
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testThatGetVenuesReturnsData {
+    getVenuesExpectation = [self expectationWithDescription:requestShouldSucceed];
+    [FSNetwork requestPlacesWithSearchQuery:searchQuery latLngQuery:latLngQuery delegate:self];
+    [self waitForExpectationsWithCommonTimeoutUsingHandler:nil];
+}
+
+#pragma mark - Networking
+
+- (void)networkDidFailWithError:(NSError *)error successSelector:(SEL)selector {
+    [getVenuesShouldFailExpectation fulfill];
+}
+
+- (void)networkDidSearchWithResults:(NSMutableArray *)results {
+    XCTAssert(results.count > 0, @"Results returned no data");
+    
+    [getVenuesExpectation fulfill];
 }
 
 @end
